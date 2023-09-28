498d92d3e38462109a24c31aaf63e11c937fd43b
jane-patricia <72210472@students.ukdw.ac.id>
2023-09-29 02:06:29 +0700

    front-end responsive

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,10 @@
 let popup = document.getElementsByClassName('popup')[0];
-let btnLogin = document.getElementById('login');
+let btnLogin = document.getElementsByClassName('button-login')[0];
 let dropClass = document.getElementsByClassName('classes-content')[0];
-let icon = document.getElementsByClassName('icon')[0];
+// let icon = document.getElementsByClassName('icon')[0];
 let nav = document.getElementById('navbar');
+let menuTgl = document.querySelector('.menu-toggle input');
+let navUl = document.querySelector('nav ul')
 
 btnLogin.addEventListener('click', (e) => {
     e.preventDefault();
@@ -14,12 +16,17 @@ document.getElementsByClassName('close')[0].addEventListener('click', () => {
     popup.style.display = 'none';
 })
 
-icon.addEventListener('click', (e) => {
-    e.preventDefault();
-    if(nav.className === "nav-menu"){
-        nav.className += " responsive";
-    }
-    else{
-        nav.className = "nav-menu";
-    }
+menuTgl.addEventListener('click', () => {
+    navUl.classList.toggle('slide');
+    // navUl.className += " slide";
 })
+
+// icon.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(nav.className === "nav-menu"){
+//         nav.className += " responsive";
+//     }
+//     else{
+//         nav.className = "nav-menu";
+//     }
+// })
