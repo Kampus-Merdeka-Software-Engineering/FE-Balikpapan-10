ffe714c8b5e547654a54993c6927e68e76375cb7
jane-patricia <72210472@students.ukdw.ac.id>
2023-10-07 16:56:38 +0700

    footer event

--- js/footer.js
+++ js/footer.js
@@ -1,19 +1,36 @@
-// const API_BASE_URL = ''
 const submit = document.getElementById('btnSubmit');
-
-// alert('hii');
 
 submit.addEventListener('click', (e) => {
     e.preventDefault();
-    // console.log('clicked');
     addMessage();
 });
+
+// const API_BASE_URL = 'https://be-balikpapan-10-production.up.railway.app';
+
+async function sendMessage() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('emailMssg').value;
+    let message = document.getElementById('message').value;
+    try {
+        const response = await fetch(`${API_BASE_URL}/message`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, message })
+        })
+        const data = await response.json();
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
 
 async function addMessage() {
     let name = document.getElementById('name').value;
     let email = document.getElementById('emailMssg').value;
     let message = document.getElementById('message').value;
-    // console.log(name, email, message)
+    console.log(name, email, message)
 
     // try {
     //     const response = await fetch(`${API_BASE_URL}/message`, {
