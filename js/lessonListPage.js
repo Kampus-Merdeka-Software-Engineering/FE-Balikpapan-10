0ff5af7c05c73394fac525f62072d13fcadfb69d
jane-patricia <72210472@students.ukdw.ac.id>
2023-10-07 11:56:33 +0700

    lessonlistchange

--- js/lessonListPage.js
+++ js/lessonListPage.js
@@ -1,24 +1,23 @@
-// const API_BASE_URL = '';
+// const API_BASE_URL = 'https://be-balikpapan-10-production.up.railway.app';
 
 // async function fetchLesson(){
 //   try {
-//     const response = await fetch(`${API_BASE_URL}/lessons`);
+//     const response = await fetch(`${API_BASE_URL}/lessonslist`);
 //     const data = await response.json();
 //     const lessonList = document.getElementById('wrapper-class');
 //     let cards = ``;
 //     data.forEach((lesson) => {
 //       cards+=`
-//       <div class="class-card" data-aos="fade-up" data-aos-duration="700" data-aos-offset="-100">
+//       <div class="class-card" data-aos="fade-up" data-aos-duration="700" data-aos-offset="-100" id="${lesson.id}>
 //       <img src="${lesson.image}" alt="Class 1" class="class1">
 //       <div class="class-info">
-//         <p class="age">Age: 5-8</p>
-//         <p class="level">Level: 3</p>
+//         <p class="age">Age: ${lesson.age}</p>
+//         <p class="level">Level: ${lesson.level}</p>
 //       </div>
-//       <h3>${lesson.lessonTitle}</h3>
+//       <h3>${lesson.title}</h3>
 //       <div class="description">
-//         <p class="desc-time">Class Time: 08:00 am - 10:00 am</p>
 //         <p class="desc-title">Description<button><i class="bi bi-chevron-down"></i></button></p>
-//         <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas neque enim nesciunt nemo nam. Quis, illo nulla suscipit adipisci nesciunt maxime debitis totam iure aperiam! Nesciunt voluptas ad accusamus harum!</p>
+//         <p class="desc">${lesson.description}</p>
 //         <button class="addLesson"><i class="bi bi-plus-lg"></i></button>
 //       </div>
 //     </div>
@@ -31,6 +30,21 @@
 //   }
 // }
 
+// fetchLesson();
+
+// const baseUrl = 'http://localhost:3000/lessons'
+// document.addEventListener('load', getLesson)
+// async function getLesson(e){
+//   window.location('/lessons')
+//   e.preventDefault();
+//   const res = await fetch(baseUrl,
+//     {
+//       method: 'GET'
+//     })
+//     console.log(res);
+// }
+
+
 const jsonFile = "./lesson.json";
 fetch(jsonFile).then((response) => {
     return response.json();
@@ -40,15 +54,14 @@ fetch(jsonFile).then((response) => {
     // for(let i=0; i<5; i++){}
     data.forEach((lesson) => {
         cards+=`
-        <div class="class-card" data-aos="fade-up" data-aos-duration="700" data-aos-offset="-100">
+        <div class="class-card" data-aos="fade-up" data-aos-duration="700" data-aos-offset="-100" id="${lesson.id_lesson}">
         <img src="${lesson.image}" alt="Class 1" class="class1">
         <div class="class-info">
-          <p class="age">Age: 5-8</p>
-          <p class="level">Level: 3</p>
+          <p class="age">Age: ${lesson.age}</p>
+          <p class="level">Level: ${lesson.level}</p>
         </div>
         <h3>${lesson.lessonTitle}</h3>
         <div class="description">
-          <p class="desc-time">Class Time: 08:00 am - 10:00 am</p>
           <p class="desc-title">Description<button><i class="bi bi-chevron-down"></i></button></p>
           <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas neque enim nesciunt nemo nam. Quis, illo nulla suscipit adipisci nesciunt maxime debitis totam iure aperiam! Nesciunt voluptas ad accusamus harum!</p>
           <button class="addLesson"><i class="bi bi-plus-lg"></i></button>
@@ -57,4 +70,49 @@ fetch(jsonFile).then((response) => {
       `
     });
     container.innerHTML = cards;
+
+    handleCard();
+    // addLesson();
 })
+
+const handleCard = () => {
+  let elementCards = document.querySelectorAll('.class-card:not(.addLesson)');
+  let addBtn = document.querySelector('.addLesson');
+  elementCards.forEach((card) => {
+    card.addEventListener('click', (e) => {
+      console.log(card.id);
+      var url = 'http:localhost'
+    });
+    // addBtn.addEventListener('click', (e) => {
+    //   e.stopPropagation();
+    //   console.log('click');
+    // })
+  })
+}
+
+// const addLesson = () => {
+//   let addBtn = document.querySelectorAll('.addLesson');
+//   addBtn.
+//   addBtn.addEventListener('click', (e) => {
+//     console.log('click');
+//   })
+// }
+
+// const btn = document.getElementsByClassName('addLesson')[0];
+// btn.addEventListener('click', createNewSubs);
+
+// async function createNewSubs(){
+//   try {
+
+//   }
+//   catch {
+
+//   }
+// }
+
+
+// let btn = document.getElementsByClassName('addLesson')[0];
+
+// btn.addEventListener('click', (e) => {
+  
+// })
